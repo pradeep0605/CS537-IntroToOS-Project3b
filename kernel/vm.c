0afc4d15000552176b93d8b33592c2721fc35b46
Pradeep Kashyap Ramaswamy <pradeep.kashyap@wisc.edu>
2016-11-03 21:14:09 -0500

    When malloc/sbrk happens at the userspace, make sure that malloc doesn't go over the currently allocated shared memory locations. So, the new address increase should be < user process's shared memory locations

--- kernel/vm.c
+++ kernel/vm.c
@@ -231,7 +231,7 @@ allocuvm(pde_t *pgdir, uint oldsz, uint newsz)
   char *mem;
   uint a;
 
-  if(newsz > USERTOP)
+  if(newsz > proc->spages_info.current_top)
     return 0;
   if(newsz < oldsz)
     return oldsz;
